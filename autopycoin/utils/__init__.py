c7ff963c7e5930d5333dfa2b4eb0bd8c79a7c415
gaetanDub <65140842+gaetanDub@users.noreply.github.com>
2021-09-30 15:31:40 +0200

    unit tests of create_interpretable_nbeats

--- autopycoin/utils/__init__.py
+++ autopycoin/utils/__init__.py
@@ -1,6 +1,7 @@
 """ This subpackage defines all metrics needed to train a deep learning.
 """
 
-from .check_data import check_infinity, layer_test
+from .check_data import check_infinity
+from .testing_utils import layer_test
 
 __all__ = ["check_infinity", "layer_test"]
