c345cb93573d27277d441e59ad220d1f9a459045
gaetan <gaetan.dubuc@epitech.eu>
2022-02-04 20:30:32 +0100

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ import os
 from setuptools import find_packages, setup
 
 # get __version__ from _version.py
-from autopycoin._version import __version__
+ver_file = os.path.join("autopycoin", "_version.py")
+with open(ver_file) as f:
+    exec(f.read())
 
 DISTNAME = "autopycoin"
 DESCRIPTION = "Deep learning models for forecasting purposes."
